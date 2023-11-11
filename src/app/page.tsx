27fabcd591c184914af3e9118a2c27ed99fb53cf
shohidul <shohidul@example.com>
2023-11-10 23:18:49 -0800

    second-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Bunner from "@/components/Bunner"
+
 export default function Home() {
   return (
     <main>
+      <Bunner/>
      <h1>new ecommerce application</h1>
     </main>
   )
---
+++ src/components/Bunner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bunner = () => {
+  return (
+    <div>Bunner</div>
+  )
+}
+
+export default Bunner;
