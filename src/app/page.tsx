1d44828f412623960ba80f9037a53e7fe8f7f030
shohidul <shohidul@example.com>
2023-11-11 11:45:08 -0800

    frist-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main>
       <Bunner/>
-     <h1>new ecommerce application</h1>
     </main>
   )
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import image from "@/assets/slider/image.png";
+import image1 from "@/assets/slider/image1.png";
+import image2 from "@/assets/slider/image2.png";
+
+
+
+export {image,image1,image2};
