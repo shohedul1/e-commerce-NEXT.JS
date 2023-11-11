a27e0b5b8ab32a6181793461a7b0c59bb25e3b6b
shohidul <shohidul@example.com>
2023-11-10 20:02:22 -0800

    second-commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
+interface Props{
+    className?: string;
+    spanClassName?: string;
+}
+const Logo = ({ className, spanClassName }: Props) => {
+    return (
+        <Link href={"/"} className={cn('text-zinc-950 underline underline-offset-4 decoration-[1px] group', className)}>
+            <span className={cn("bg-zinc-950 text-white w-8 h-8 rounded-full inline-flex items-center justify-center text-2xl font-bold mr-1 group-hover:bg-blue-700 duration-200", spanClassName)}>
+                A
+            </span>mazonpro
+        </Link>
+    )
+}
+
+export default Logo
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import Logo from './Logo';
+import Link from 'next/link';
 
 const Navbar = () => {
+  const navigation =[
+    {_id: 910, title: "Home", href: "/"},
+    {_id: 911, title: "Phones", href: "/phones"},
+    {_id: 912, title: "Phone Cases", href: "/phonecases"},
+    {_id: 913, title: "Watches", href: "/watches"},
+    {_id: 914, title: "Accessories", href: "/accessories"},
+  ];
   return (
-    <div>Navbar</div>
+    <div className='w-full h-20 border-b-[1px] border-b-zinc-500 bg-white text-zinc-600'>
+        <div className='max-w-screen-xl mx-auto h-full flex items-center justify-between px-4 xl:px-0'>
+          {/* {logo} */}
+          <Logo/>
+          {/* {Navigation} */}
+           <ul className='hidden md:flex items-center gap-5 text-sm uppercase font-semibold'>
+            {
+              navigation.map((item)=>(
+                <Link href={item?.href}>
+                <li>{item?.title}</li>
+                
+                </Link>
+              ))
+            }
+           </ul>
+          {/* {icons} */}
+          <p>icons</p>
+        </div>
+    </div>
   )
 }
 
