2828673fa149f8c40b63d60304cb42c82e0f96b9
shohidul <shohidul@example.com>
2023-11-12 23:02:16 -0800

    frist

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github"; 
+// import NextAuth from "next-auth/next";
+// import GithubProvider from "next-auth/providers/github"; 
 
-const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    // ...add more providers here
-  ],
-};
+// const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID!,
+//       clientSecret: process.env.GITHUB_SECRET!,
+//     }),
+//     // ...add more providers here
+//   ],
+// };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST};
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST};
 
