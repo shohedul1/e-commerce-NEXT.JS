d207129569c3ef18d1cbf348f1cbe476332f9376
shohidul <shohidul@example.com>
2023-11-14 07:14:19 -0800

    shohidul

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,10 +5,6 @@ import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -18,4 +14,5 @@ const authOptions = {
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST};
+
 
